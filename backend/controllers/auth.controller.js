dc8467c0ce485a3066bcbb82a0b122f5c4ead063
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-07 01:08:53 +0200

    backend: added the register function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,3 +2,31 @@ import { User } from "../models/user.model";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
+export const register = async (req, res) => {
+    const {username, email, password} = req.body;
+
+    try {
+        if(!username || !email || !password){
+            return res.status(500).send({
+                message: "All fields are required",
+            });
+        }
+
+        const hashed = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            username,
+            email,
+            password: hashed,
+        });
+
+        return res.json(user);
+    }
+    catch (error){
+        console.log(error.message);
+
+        return res.status(500).send({
+            message: "Something went wrong",
+        });
+    }
+};
