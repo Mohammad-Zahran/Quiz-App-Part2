2be4c472806bafc25445d9a5c7260deebf8a5347
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-07 01:13:08 +0200

    backend: added the authRoutes to the index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import connectToDatabase from "./db/connection.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/auth", authRoutes);
 
 app.listen(8080, () => {
     console.log("Server running on port 8080");
